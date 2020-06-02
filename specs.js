4f9a8e16013fbe8c104d2d09deedca9ecbd0abde
marchuanv <marchuanv@gmail.com>
2020-06-02 15:07:28 +0200

    refactor

--- specs.js
+++ specs.js
@@ -1,2 +1,5 @@
-const emailspecs = require("./specs/email.spec.js");
-emailspecs.when_asking_a_client_for_an_emailaddress_given_on_site_it_should_check_if_it_is_correct();
+const emailvalidationspecs = require("./specs/emailvalidation.spec.js");
+for(const prop in emailvalidationspecs){
+    const func = emailvalidationspecs[prop];
+    func();
+};
---
+++ specs/emailvalidation.spec.js
@@ -0,0 +1,118 @@
+
+const EmailAddressValidation = require("../lib/emailaddressvalidation.js");
+
+module.exports = {
+    when_asking_a_client_for_an_emailaddress_given_on_site_it_should_check_if_there_is_an_at_symbol: () => {
+        
+        //arrange
+        const emailAddressValidation = new EmailAddressValidation("clientA@gmail.com");
+        let passed = true;
+        let errors = "";
+
+        //act
+        try {
+            passed = emailAddressValidation.hasAtSymbol();
+        } catch (err) {
+            errors = err;
+            passed = false;
+        }
+
+        //assert
+        if (!passed){
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+
+    },
+    when_asking_a_client_for_an_emailaddress_given_not_on_site_it_should_check_if_it_is_correct: () => {
+        
+        //arrange
+        const emailAddressValidation = new EmailAddressValidation("clientA@gmail.com");
+        let passed = true;
+        let errors = "";
+
+        //act
+        try {
+            passed = emailAddressValidation.hasAtSymbol();
+            if (passed){
+                passed = emailAddressValidation.hasSecondLevelDomain();
+            }
+            if (passed){
+                passed = emailAddressValidation.hasTopLevelDomain();
+            }
+        } catch (err) {
+            errors = err;
+            passed = false;
+        }
+
+        //assert
+        if (!passed){
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_an_emailaddress_given_on_site_it_should_check_if_there_is_a_second_level_domain: () => {
+        
+        //arrange
+        const emailAddressValidation = new EmailAddressValidation("clientA@gmail.com");
+        let passed = true;
+        let errors = "";
+
+        //act
+        try {
+            passed = emailAddressValidation.hasSecondLevelDomain();
+        } catch (err) {
+            errors = err;
+            passed = false;
+        }
+
+        //assert
+        if (!passed){
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_an_invalid_emailaddress_given_on_site_it_should_check_if_there_is_a_second_level_domain: () => {
+        
+        //arrange
+        const emailAddressValidation = new EmailAddressValidation("clientAgmail.com");
+        let passed = true;
+        let errors = "";
+
+        //act
+        try {
+            passed = emailAddressValidation.hasSecondLevelDomain();
+        } catch (err) {
+            errors = err;
+            passed = false;
+        }
+
+        //assert
+        if (passed){
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_an_emailaddress_given_on_site_it_should_check_if_there_is_a_top_level_domain: () => {
+        
+        //arrange
+        const emailAddressValidation = new EmailAddressValidation("clientA@gmail.com");
+        let passed = true;
+        let errors = "";
+
+        //act
+        try {
+            passed = emailAddressValidation.hasTopLevelDomain();
+        } catch (err) {
+            errors = err;
+            passed = false;
+        }
+
+        //assert
+        if (!passed){
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+
+    }
+}
