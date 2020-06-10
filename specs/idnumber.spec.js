6a6a4e5d83af99e611a387c7f6d91fc5e5503377
marchuanv <marchuanv@gmail.com>
2020-06-10 13:05:48 +0200

    finished id number tests

--- specs/idnumber.spec.js
+++ specs/idnumber.spec.js
@@ -17,7 +17,7 @@ module.exports = {
         }
 
         //assert
-        if (!passed) {
+        if (!passed || passed === undefined) {
             const message = `TEST FAILED: ${errors}`;
             throw new Error(message);
         }
@@ -80,7 +80,7 @@ module.exports = {
         }
 
         //assert
-        if (!passed) {
+        if (!passed || passed === undefined) {
             const message = `TEST FAILED: ${errors}`;
             throw new Error(message);
         }
@@ -110,20 +110,20 @@ module.exports = {
     when_asking_a_client_for_their_id_number_given_on_site_and_the_7th_to_10th_digits_are_over_5000_it_should_determine_male_gender: () =>{
 
         //arrange
-        let gender = "";
+        let passed = false;
         let errors = "";
         let idNumberValidation = new IdNumberValidation("7607046305087");
 
         //act
         try {
-            gender = idNumberValidation.getGender();
+            passed = idNumberValidation.isMale();
         } catch (err) {
-            gender = "";
+            passed = false;
             errors = err;
         }
 
         //assert
-        if (gender !== "male") {
+        if (!passed || passed === undefined) {
             const message = `TEST FAILED: ${errors}`;
             throw new Error(message);
         }
@@ -131,41 +131,146 @@ module.exports = {
     when_asking_a_client_for_their_id_number_given_on_site_and_the_7th_to_10th_digits_are_below_5000_it_should_determine_female_gender: () =>{
 
        //arrange
-       let gender = "";
+       let passed = false;
        let errors = "";
        let idNumberValidation = new IdNumberValidation("7607042654082");
 
-       //act
-       try {
-           gender = idNumberValidation.getGender();
-       } catch (err) {
-            gender = "";
-            errors = err;
-       }
-
-       //assert
-       if (gender !== "female") {
-           const message = `TEST FAILED: ${errors}`;
-           throw new Error(message);
-       }
-    },
-    when_asking_a_client_for_their_id_number_given_on_site_and_it_is_not_valid_it_should_fail: () =>{
-
-        //arrange
-        let failed = true;
-        let errors = "";
-        let idNumberValidation = new IdNumberValidation(1234567890123);
-
         //act
         try {
-            failed = idNumberValidation.isValid();
+            passed = idNumberValidation.isFemale();
         } catch (err) {
-            failed = false;
+            passed = false;
             errors = err;
         }
 
         //assert
-        if (failed) {
+        if (!passed || passed === undefined) {
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_their_id_number_given_on_site_it_should_determine_if_you_are_a_south_african_citizen: () =>{
+
+        //arrange
+        let passed = true;
+        let errors = "";
+        let idNumberValidation = new IdNumberValidation("9101059236080");
+
+        //act
+        try {
+            passed = idNumberValidation.isSouthAfricanCitizen();
+        } catch (err) {
+            passed = false;
+            errors = err;
+        }
+
+        //assert
+        if (!passed || passed === undefined) {
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_their_id_number_given_on_site_it_should_determine_if_you_are_a_foreign_citizen: () =>{
+
+        //arrange
+        let failed = true;
+        let errors = "";
+        let idNumberValidation = new IdNumberValidation(8602026186184);
+
+        //act
+        try {
+            failed = idNumberValidation.isSouthAfricanCitizen();
+        } catch (err) {
+            failed = true;
+            errors = err;
+        }
+
+        //assert
+        if (failed || failed === undefined) {
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_their_id_number_given_on_site_it_should_determine_if_you_are_a_foreign_citizen: () =>{
+
+        //arrange
+        let failed = true;
+        let errors = "";
+        let idNumberValidation = new IdNumberValidation(8602026186184);
+
+        //act
+        try {
+            failed = idNumberValidation.isSouthAfricanCitizen();
+        } catch (err) {
+            failed = true;
+            errors = err;
+        }
+
+        //assert
+        if (failed || failed === undefined) {
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_their_id_number_given_on_site_and_it_is_valid_it_should_run_a_sequence_check: () =>{
+
+        //arrange
+        let passed = false;
+        let errors = "";
+        let idNumberValidation = new IdNumberValidation(8602026186184);
+
+        //act
+        try {
+            passed = idNumberValidation.isValidSequence();
+        } catch (err) {
+            passed = false;
+            errors = err;
+        }
+
+        //assert
+        if (!passed || passed === undefined) {
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_their_id_number_given_on_site_and_it_is_not_valid_it_should_run_a_sequence_check: () =>{
+
+        //arrange
+        let failed = true;
+        let errors = "";
+        let idNumberValidation = new IdNumberValidation(8602026186111);
+
+        //act
+        try {
+            failed = idNumberValidation.isValidSequence();
+        } catch (err) {
+            failed = true;
+            errors = err;
+        }
+
+        //assert
+        if (failed || failed === undefined) {
+            const message = `TEST FAILED: ${errors}`;
+            throw new Error(message);
+        }
+    },
+    when_asking_a_client_for_their_id_number_given_on_site_and_it_is_valid_it_should_pass: () =>{
+
+        //arrange
+        let passed = true;
+        let errors = "";
+        let idNumberValidation = new IdNumberValidation("8602026911086");
+
+        //act
+        try {
+            passed = idNumberValidation.isValid();
+        } catch (err) {
+            passed = false;
+            errors = err;
+        }
+
+        //assert
+        if (!passed || passed === undefined) {
             const message = `TEST FAILED: ${errors}`;
             throw new Error(message);
         }
